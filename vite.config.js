3d0ce4f1aea048fe1c5e7f0ca6fdc2e06a7d945d
Carlos D Walker <60205826+Mrwealthyexp@users.noreply.github.com>
2025-06-04 00:22:13 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['wagmi', 'viem', '@rainbow-me/rainbowkit', 'ethers']
-  }
-});
